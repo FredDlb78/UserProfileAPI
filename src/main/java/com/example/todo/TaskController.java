bccfca42c50c3caa9bbdbaa000ac12f5fb8ad87f
fdelabre-accor <frederic.delabre@consulting-for.accor.com>
2025-07-23 21:02:29 +0200

    Ajout de la doc des erreurs

--- src/main/java/com/example/todo/TaskController.java
+++ src/main/java/com/example/todo/TaskController.java
@@ -1,5 +1,10 @@
 package com.example.todo;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +23,25 @@ public class TaskController {
 
 
     @GetMapping("/tasks")
+    @Operation(summary = "Get all tasks")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "This is the list of all tasks",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Task.class))),
+    })
     public List<Task> getAllTasks() {
         return tasks;
     }
 
     @PostMapping("/tasks")
+    @Operation(summary = "Create a task")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Task successfully created",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Task.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request",
+                    content = @Content) // 👈 aucun corps
+    })
     public ResponseEntity<Task> createTask(@RequestBody Task newTask) {
         newTask.setId(nextId);
         nextId++;
@@ -31,6 +50,16 @@ public class TaskController {
     }
 
     @PutMapping("/tasks/{id}")
+    @Operation(summary = "Update a task by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Task successfully updated",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Task.class))),
+            @ApiResponse(responseCode = "404", description = "Task not found",
+                    content = @Content), // 👈 aucun corps
+            @ApiResponse(responseCode = "400", description = "Invalid request",
+                    content = @Content) // 👈 aucun corps
+    })
     public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updatedTask) {
         for (Task task : tasks) {
             if (task.getId().equals(id)) {
@@ -43,6 +72,14 @@ public class TaskController {
     }
 
     @DeleteMapping("/tasks/{id}")
+    @Operation(summary = "Delete a task by its ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Task successfully deleted",
+                    content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Task.class))),
+            @ApiResponse(responseCode = "404", description = "Task not found",
+                    content = @Content), // 👈 aucun corps
+    })
     public ResponseEntity<Task> deleteTask(@PathVariable Long id) {
         boolean removed = tasks.removeIf(task -> task.getId().equals(id));
         if (removed) {
