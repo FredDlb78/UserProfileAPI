e5ff1e9cb682b694e4af698c3fa6e3c0fec700f4
fdelabre-accor <frederic.delabre@consulting-for.accor.com>
2025-08-15 10:29:49 +0200

    Ajout d'un commandLineRunner pour démo

--- src/main/java/com/example/todo/UserProfileApplication.java
+++ src/main/java/com/example/todo/UserProfileApplication.java
@@ -1,7 +1,10 @@
 package com.example.todo;
 
+import com.example.todo.repository.UserProfileRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserProfileApplication {
@@ -10,4 +13,23 @@ public class UserProfileApplication {
 		SpringApplication.run(UserProfileApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(UserProfileRepository repository) {
+		return args -> {
+			if (repository.count() == 0) {
+				repository.save(new UserProfile(
+						null,
+						"Fredéric",
+						"Delabre",
+						"QA Automation Engineer",
+						5,
+						"Java/Selenium/Appium, backend & IA."
+				));
+				System.out.println("Profiled inserted!");
+			}
+			repository.findAll().forEach(
+					profile -> System.out.println("Profile found : " + profile.getFullName())
+			);
+		};
+	}
 }
