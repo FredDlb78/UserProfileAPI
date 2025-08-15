679b16b4bce1cc180d9bf816848a5776bc3df2b2
fdelabre-accor <frederic.delabre@consulting-for.accor.com>
2025-08-15 10:29:19 +0200

    Utilisation de Lombok pour les annotations

--- src/main/java/com/example/todo/UserProfile.java
+++ src/main/java/com/example/todo/UserProfile.java
@@ -1,68 +1,32 @@
 package com.example.todo;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user_profiles")
+@Data // génère getters, setters, toString, equals, hashCode
+@NoArgsConstructor // constructeur vide
+@AllArgsConstructor // constructeur avec tous les champs
 public class UserProfile {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 150)
     private String fullName;
+
+    @Column(length = 120)
     private String title;
+
+    @Column(length = 1000)
     private String description;
+
     private Integer experienceYears;
+
+    @Column(length = 255)
     private String technicalStack;
-
-    public UserProfile(Long id,String fullName, String title, String description, Integer experienceYears, String technicalStack) {
-        this.id = id;
-        this.fullName = fullName;
-        this.title = title;
-        this.description = description;
-        this.experienceYears = experienceYears;
-        this.technicalStack = technicalStack;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getExperienceYears() {
-        return experienceYears;
-    }
-
-    public void setExperienceYears(Integer experienceYears) {
-        this.experienceYears = experienceYears;
-    }
-
-    public String getTechnicalStack() {
-        return technicalStack;
-    }
-
-    public void setTechnicalStack(String technicalStack) {
-        this.technicalStack = technicalStack;
-    }
-
 }
